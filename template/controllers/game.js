f4f27766427dd161a4761ebb523a1a4c31fc6c77
RuderWest <91721895+matheobernabe@users.noreply.github.com>
2024-04-25 19:54:44 +0200

    Création de partie

--- template/controllers/game.js
+++ template/controllers/game.js
@@ -1,4 +1,4 @@
-const { createGame, joinGame } = require("../models/game.js");
+const Game = require("../models/game.js");
 // game.js
 
 // Stockage des parties en cours
@@ -6,21 +6,28 @@ const activeGames = {
     
 };
 module.exports = {
-
-    createGame: async(req, res, next) => {
-        const creatorId = req.body;
-        const gameId = generateGameId(); // Générez un identifiant unique pour la partie
-        const newGame = {
-            id: gameId,
-            player1: creatorId, // Le créateur est le joueur 1
-            player2: null, // Le joueur 2 n'a pas encore rejoint
-            board: initializeBoard(), // Initialiser le plateau de jeu (tableau vide)
-        };
-        activeGames[gameId] = newGame;
-        return gameId;
+    iget: async (req, res, next) => {
+        
+        const game = await Game.findAll();
+        if (game) res.json(game);
+        else res.sendStatus(404);
     },
-
-    // Rejoindre une partie existante
+    createGame: async (req, res, next) => {
+        try {
+            const creatorId = req.body;
+            const gameId = generateGameId(); // Générez un identifiant unique pour la partie
+            const newGame = {
+                id: gameId,
+                player1: creatorId, // Le créateur est le joueur 1
+                player2: null, // Le joueur 2 n'a pas encore rejoint
+                board: initializeBoard(), // Initialiser le plateau de jeu (tableau vide)
+            };
+            activeGames[gameId] = newGame;
+            res.status(201).json(newGame); // Envoyer la nouvelle partie au client
+        } catch (error) {
+            next(error);
+        }
+    },
     joinGame(gameId, playerId) {
         if (activeGames[gameId]) {
             activeGames[gameId].player2 = playerId; // Le joueur 2 rejoint la partie
@@ -29,18 +36,11 @@ module.exports = {
         return false; // La partie n'existe pas
     },
 
-    // Fonction utilitaire pour générer un identifiant unique
-    generateGameId() {
-        // Implémentez votre propre logique pour générer un ID unique
-        // Par exemple, vous pouvez utiliser un horodatage ou un UUID
-        // Pour simplifier, nous utilisons un nombre aléatoire ici
-        return Math.floor(Math.random() * 1000);
-    },
 
-    // Initialiser un plateau de jeu vide
-    initializeBoard() {
-        return ["", "", "", "", "", "", "", "", ""];
-    }
-
-
+};
+function generateGameId() {
+    return Math.floor(Math.random() * 1000);
+};
+function initializeBoard() {
+    return ["", "", "", "", "", "", "", "", ""];
 };
--- template/routes/game.js
+++ template/routes/game.js
@@ -12,6 +12,9 @@ router.post("/create", GameController.createGame);
 //router.post("/join/:gameId", game.joinGame);
 router.post("/join/:gameId", GameController.joinGame);
 
+// Item route : GET : fetch an user
+router.get( /* middlewares */ GameController.iget);
+
 
 // Autres routes spécifiques au jeu (par exemple, jouer un coup, obtenir l'état du plateau, etc.)
 
